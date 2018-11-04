e484f0a569a6fb8844995e9994f5eed9e27660b8
Mladen Kolar <mkolar@midway2-login1.rcc.local>
2018-11-04 00:14:10 -0500

    updates

--- exper/experiment1_compute_coverage.jl
+++ exper/experiment1_compute_coverage.jl
@@ -1,16 +1,26 @@
 using KLIEPInference
 using JLD
 
-NUM_REP = 1000
+
 coverage = 0
 coverage_stud = 0
 
-p = 10
-sgn = 0
-nx = 500
-ny = 500
+# p = 10
+# sgn = 0
+# nx = 500
+# ny = 500
+
+p = parse(Int,ARGS[1])
+sgn = parse(Int, ARGS[2])
+nx = parse(Int,ARGS[3])
+ny = parse(Int,ARGS[4])
+NUM_REP = parse(Int,ARGS[5])
+
+
 
 for rep=1:NUM_REP
+    global coverage
+    global coverage_stud
 
     fname = "/scratch/midway2/mkolar/KLIEP/exp1/res_$(p)_$(sgn)_$(nx)_$(ny)_$(rep).jld"
 
@@ -21,7 +31,7 @@ for rep=1:NUM_REP
     CI = simulCI(res)
     coverage += all(0 .<= CI[:, 2]) * all(0 .>= CI[:, 1]) ? 1 : 0
 
-    CI = simulCI(simulCIstudentized)
+    CI = simulCIstudentized(res)
     coverage_stud += all(0 .<= CI[:, 2]) * all(0 .>= CI[:, 1]) ? 1 : 0
 end
 
--- exper/experiment1_generate_graph.jl
+++ exper/experiment1_generate_graph.jl
@@ -8,6 +8,5 @@ sgn = parse(Int,ARGS[2])
 
 θx = chain(p, 5, 0.2, 0.4, sgn)
 θy = copy(θx)
-Δ = θx - θy
 
 @save "params_exp1_$(p)_$(sgn).jld" p sgn θx θy
