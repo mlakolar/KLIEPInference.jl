7a6d6463a2bd9d36cc69043d2ba64c7941dae539
Byol Kim <>
2021-01-17 15:53:18 -0600

    removed vector biasing for KLIEP_debias1

--- src/debias.jl
+++ src/debias.jl
@@ -14,22 +14,6 @@ function KLIEP_debias1(Ψx, Ψy, θ, ω, idx::Int64)
     θ1
 end
 
-function KLIEP_debias1(Ψx, Ψy, θ, ω, idx::Int64, g)
-    μx = vec(mean(Ψx, dims=2))
-
-    r = zeros(size(Ψy, 2))
-    mul!(r, transpose(Ψy), θ)
-    r .= exp.(r)
-    r ./= mean(r)
-
-    θ1 = θ[idx]
-    for k in findall(!iszero, ω)
-        θ1 += ω[k] * ( μx[k] - mean( r .* Ψy[k, :] ) - g[k] )
-    end
-
-    θ1
-end
-
 function KLIEP_debias2(Ψx, Ψy, θ, ω, idx::Int64)
     supp = sort(union(idx, findall(!iszero, θ), findall(!iszero, ω)))
 
