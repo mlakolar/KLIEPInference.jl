633c28e9230561531951a6f622c6e6c0aec415f2
Byol Kim <>
2021-02-17 15:06:25 -0600

    re-organizing: fixed a typo

--- src/debias.jl
+++ src/debias.jl
@@ -1,6 +1,6 @@
 function _debias1(Ψx, Ψy, θ, ω, θ_ind::Int)
     μx = vec(mean(Ψx, dims=2))
-    supp3 = _findSupp3(θ, [], θ_ind)
+    supp3 = _find_supp3(θ, [], θ_ind)
     θk = KLIEP(Ψx[supp3, :], Ψy[supp3, :], CD_KLIEP())
     r = rhat(θk, Ψy[supp3, :])
     θ1 = θk[end]
@@ -14,7 +14,7 @@ function _debias1(Ψx, Ψy, θ, Hinv, θ_ind::Union{Vector{Int},UnitRange})
     θ1 = Vector{Float64}(undef, length(θ_ind))
     μx = vec(mean(Ψx, dims=2))
     for k in 1:length(θ_ind)
-        supp3 = _findSupp3(θ, [], θ_ind[k])
+        supp3 = _find_supp3(θ, [], θ_ind[k])
         θk = KLIEP(Ψx[supp3, :], Ψy[supp3, :], CD_KLIEP())
         r = rhat(θk, Ψy[supp3, :])
         θ1[k] = θk[end]
@@ -26,7 +26,7 @@ function _debias1(Ψx, Ψy, θ, Hinv, θ_ind::Union{Vector{Int},UnitRange})
 end
 
 function _debias2(Ψx, Ψy, θ, ω, θ_ind::Int)
-    supp3 = _findSupp3(θ, ω, θ_ind)
+    supp3 = _find_supp3(θ, ω, θ_ind)
     θk = KLIEP(Ψx[supp3,:], Ψy[supp3,:], CD_KLIEP())
     θk[end]
 end
@@ -34,7 +34,7 @@ end
 function _debias2(Ψx, Ψy, θ, Hinv, θ_ind::Union{Vector{Int},UnitRange})
     θ2 = Vector{Float64}(undef, length(θ_ind))
     for k in 1:length(θ_ind)
-        supp3 = _findSupp3(θ, Hinv[k], θ_ind[k])
+        supp3 = _find_supp3(θ, Hinv[k], θ_ind[k])
         θk = KLIEP(Ψx[supp3, :], Ψy[supp3, :], CD_KLIEP())
         θ2[k] = θk[end]
     end
