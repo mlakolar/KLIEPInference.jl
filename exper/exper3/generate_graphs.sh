845442cf0baab7ca90ea6e3529499092e1af89f3
anewgithubname <song.liu@bristol.ac.uk>
2021-03-18 19:10:45 +0000

    changed result folder to ./res

--- exper/exper3/generate_graphs.sh
+++ exper/exper3/generate_graphs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+alias jl='julia'
 
 for m in "25" "50" "100"
 do
   for sgn in "-1" "0" "1"
   do
-    echo "jl exp3_generate_graph.jl ${m} ${sgn} ..."
-    jl exp3_generate_graph.jl ${m} ${sgn}
+    echo "jl exp3_generate_graphs.jl ${m} ${sgn} ..."
+    jl exp3_generate_graphs.jl ${m} ${sgn}
     echo "... done"
   done
 done
