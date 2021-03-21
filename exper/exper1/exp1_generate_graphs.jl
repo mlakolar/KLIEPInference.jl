f845d75771858a826c1ff276fe032f0b2842e5ce
Byol Kim <>
2021-03-20 23:04:24 -0500

    generating graphs for exper1 and exper2

--- exper/exper1/exp1_generate_graphs.jl
+++ exper/exper1/exp1_generate_graphs.jl
@@ -1,5 +1,6 @@
 using KLIEPInference
 using Random
+using Distributions
 using JLD
 
 Random.seed!(325)
@@ -11,25 +12,16 @@ idx2 = KLIEPInference.trimap(6, 7)
 idx3 = KLIEPInference.trimap(4, 6)
 idx4 = KLIEPInference.trimap(5, 7)
 
-# m = 25
-γx = chain(25, 25, 0.1, 0.3, 0)
-γy = deepcopy(γx)
-γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
-γy[idx1] *= -1.0
-γy[idx2] *= -1.0
-γy[idx3] -= 0.1
-γy[idx4] -= 0.1
-@save "graphs/params_exp1_chain1_25.jld" γx γy
-
-# m = 50
-γx = chain(50, 50, 0.1, 0.3, 0)
-γy = deepcopy(γx)
-γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
-γy[idx1] *= -1.0
-γy[idx2] *= -1.0
-γy[idx3] -= 0.1
-γy[idx4] -= 0.1
-@save "graphs/params_exp1_chain1_50.jld" γx γy
+for m in [25, 50]
+    γx = chain(m; lenC=m, sgn=0)
+    γy = deepcopy(γx)
+    γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
+    γy[idx1] *= -1.0
+    γy[idx2] *= -1.0
+    γy[idx3] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    γy[idx4] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    @save "graphs/params_exp1_chain1_$(m).jld" γx γy
+end
 
 ## chain (2)
 idx0 = KLIEPInference.trimap(5, 6)
@@ -38,22 +30,51 @@ idx2 = KLIEPInference.trimap(7, 8)
 idx3 = KLIEPInference.trimap(4, 5)
 idx4 = KLIEPInference.trimap(4, 6)
 
-# m = 25
-γx = chain(25, 25, 0.1, 0.3, 0)
-γy = deepcopy(γx)
-γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
-γy[idx1] *= -1.0
-γy[idx2] *= -1.0
-γy[idx3] -= 0.1
-γy[idx4] -= 0.1
-@save "graphs/params_exp1_chain2_25.jld" γx γy
+for m in [25, 50]
+    γx = chain(m; lenC=m, sgn=0)
+    γy = deepcopy(γx)
+    γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
+    γy[idx1] *= -1.0
+    γy[idx2] *= -1.0
+    γy[idx3] = abs(γy[idx3]) > 0.2 ? γy[idx3] - sign(γy[idx3])*0.1 : γy[idx3] + sign(γy[idx3])*0.1
+    γy[idx4] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    @save "graphs/params_exp1_chain2_$(m).jld" γx γy
+end
 
-# m = 50
-γx = chain(50, 50, 0.1, 0.3, 0)
-γy = deepcopy(γx)
-γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
-γy[idx1] *= -1.0
-γy[idx2] *= -1.0
-γy[idx3] -= 0.1
-γy[idx4] -= 0.1
-@save "graphs/params_exp1_chain2_50.jld" γx γy
+Random.seed!(324)
+
+## tree (1)
+idx0 = KLIEPInference.trimap(1, 3)
+idx1 = KLIEPInference.trimap(1, 2)
+idx2 = KLIEPInference.trimap(3, 8)
+idx3 = KLIEPInference.trimap(1, 9)
+idx4 = KLIEPInference.trimap(3, 4)
+
+for m in [25, 50]
+    γx = kary_tree(m, sgn=0)
+    γy = deepcopy(γx)
+    γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
+    γy[idx1] *= -1.0
+    γy[idx2] *= -1.0
+    γy[idx3] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    γy[idx4] = abs(γy[idx4]) > 0.2 ? γy[idx4] - sign(γy[idx4])*0.1 : γy[idx4] + sign(γy[idx4])*0.1
+    @save "graphs/params_exp1_tree1_$(m).jld" γx γy
+end
+
+## tree (2)
+idx0 = KLIEPInference.trimap(1, 3)
+idx1 = KLIEPInference.trimap(2, 5)
+idx2 = KLIEPInference.trimap(8, 23)
+idx3 = KLIEPInference.trimap(2, 3)
+idx4 = KLIEPInference.trimap(3, 4)
+
+for m in [25, 50]
+    γx = kary_tree(m, sgn=0)
+    γy = deepcopy(γx)
+    γy[idx0] = abs(γy[idx0]) > 0.2 ? γy[idx0] - sign(γy[idx0])*0.1 : γy[idx0] + sign(γy[idx0])*0.1
+    γy[idx1] *= -1.0
+    γy[idx2] *= -1.0
+    γy[idx3] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    γy[idx4] += (2. * rand(Bernoulli()) - 1.) * 0.1
+    @save "graphs/params_exp1_tree2_$(m).jld" γx γy
+end
--- exper/exper2/exp2_generate_graphs.jl
+++ exper/exper2/exp2_generate_graphs.jl
@@ -8,7 +8,7 @@ close(file)
 # setting 1. (none)
 idx0 = KLIEPInference.trimap(5, 6)
 for δ in -0.75:0.05:0.75
-    γx = γy
+    γx = deepcopy(γy)
     γx[idx0] += δ
     @save "graphs/params_exp2_set1_$(δ).jld" γx γy
 end
@@ -17,7 +17,7 @@ end
 idx1 = KLIEPInference.trimap(4, 5)
 idx2 = KLIEPInference.trimap(6, 7)
 for δ in -0.75:0.05:0.75
-    γx = γy
+    γx = deepcopy(γy)
     γx[idx0] += δ
     γx[idx1] += 0.4
     γx[idx2] -= 0.4
@@ -28,7 +28,7 @@ end
 idx3 = KLIEPInference.trimap(4, 6)
 idx4 = KLIEPInference.trimap(5, 7)
 for δ in -0.75:0.05:0.75
-    γx = γy
+    γx = deepcopy(γy)
     γx[idx0] += δ
     γx[idx3] += 0.2
     γx[idx4] += 0.2
@@ -37,7 +37,7 @@ end
 
 # setting 4. (mixed)
 for δ in -0.75:0.05:0.75
-    γx = γy
+    γx = deepcopy(γy)
     γx[idx0] += δ
     γx[idx1] += 0.4
     γx[idx2] -= 0.4
