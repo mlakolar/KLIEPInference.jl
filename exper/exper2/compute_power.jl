050b6e55bcf02b517d21489270d1ab1457851a49
Mladen Kolar <mkolar@gmail.com>
2021-03-23 00:06:26 -0500

    update

--- exper/exper2/compute_power.jl
+++ exper/exper2/compute_power.jl
@@ -9,25 +9,25 @@ for set in ["set1", "set2", "set3", "set4"]
     for del in -0.75:0.05:0.75
         i += 1
         nrep = 0
-        for rep in 1:1000
+        for batch in 1:10
             try
-                file = jldopen("$(scratch_dir)/exp2_$(set)_$(@sprintf("%.2f", del))/res_$(set)_$(del)_$(rep).jld", "r")
+                file = jldopen("$(scratch_dir)/res_$(set)_$(del)_$(batch).jld", "r")
                 T1 = read(file, "T1")
                 T2 = read(file, "T2")
                 close(file)
 
-                power[1, i] += T1
-                power[2, i] += T2
+                power[1, i] += sum(T1)
+                power[2, i] += sum(T2)
 
                 nrep += 1
             catch
-                @warn "Could not open rep $(set) $(del) $(rep)"
+                @warn "Could not open rep $(set) $(del) $(batch)"
             end
         end
-	power[1, i] = power[1, i] / nrep
-	power[2, i] = power[2, i] / nrep
+	    power[1, i] = power[1, i] / (nrep * 100)
+	    power[2, i] = power[2, i] / (nrep * 100)
     end
-
 
     @save "power_$(set).jld" power
 end
+
--- exper/exper2/experiment2.jl
+++ exper/exper2/experiment2.jl
@@ -1,8 +1,10 @@
-set = "set1"
-δ = 0.0
-scratch_dir = "tmp"
+set = ARGS[1]
+δ = parse(Float64, ARGS[2])
+batch = parse(Int, ARGS[3])
+#rep = parse(Int, ARGS[3])
+scratch_dir = ARGS[4]   # e.g. "/scratch/midway2/byolkim/exper2"
 
-if isfile("$(scratch_dir)/res_$(set)_$(δ)_$(rep).jld")
+if isfile("$(scratch_dir)/res_$(set)_$(δ)_$(batch).jld")
     println("the file already exists!")
     exit()
 end
@@ -12,55 +14,86 @@ using ProximalBase, CoordinateDescent
 using LinearAlgebra, SparseArrays, Statistics, Random
 using Distributions, StatsBase, JLD
 
-println("importing parameters from params_exp2_$(set)_$(δ).jld")
-file = jldopen("graphs/params_exp2_$(set)_$(δ).jld", "r")
-γx = read(file, "γx")
-γy = read(file, "γy")
-close(file)
+function generate_data(γx, γy, rep)
+    nx = 150
+    ny = 300
 
-idx = KLIEPInference.trimap(5, 6)
+    println("Repetition $(rep)")
+    println("=================")
+    println("generating samples")
+    Random.seed!(123 + rep)
+    spl = IsingSampler(γx; thin=2000)
+    X = rand(spl, nx)
+    spl = IsingSampler(γy; thin=2000)
+    Y = rand(spl, ny)
 
-p = length(γx)
-nx = 150
-ny = 300
+    Ψx = Ψising(X)
+    Ψy = Ψising(Y)
 
-println("generating samples")
-Random.seed!(123 + rep)
-spl = IsingSampler(γx; thin=2000)
-X = rand(spl, nx)
-spl = IsingSampler(γy; thin=2000)
-Y = rand(spl, ny)
+    Ψx, Ψy
+end
 
-Ψx = Ψising(X)
-Ψy = Ψising(Y)
+function test_exper2(Ψx, Ψy, p)
+    nx = 150
+    ny = 300
+    idx = KLIEPInference.trimap(5, 6)    
 
-println("step 1")
-λ1 = 1.01 * quantile(Normal(), 1. - 0.05 / p)
-θ = spKLIEP(Ψx, Ψy, λ1, CD_KLIEP(); loadings=true)
+    println("step 1")
+    λ1 = 1.01 * quantile(Normal(), 1. - 0.05 / p)
+    θ = spKLIEP(Ψx, Ψy, λ1, CD_KLIEP(); loadings=true)
 
-println("step 2")
-λ2 = sqrt(2. * log(p) / ny)
-H = KLIEP_Hessian(θ, Ψy)
-ω = Hinv_row(H, idx, λ2)
-supp = KLIEPInference._find_supp(idx, ω)
-ω[supp] = view(H, supp, supp)\(supp .=== idx)
+    println("step 2")
+    λ2 = sqrt(2. * log(p) / ny)
+    H = KLIEP_Hessian(θ, Ψy)
+    ω = Hinv_row(H, idx, λ2)
+    supp = KLIEPInference._find_supp(idx, ω)
+    ω[supp] = view(H, supp, supp)\(supp .=== idx)
 
-println("step 3")
-# SparKLIE+1
-θ1 = KLIEPInference._debias1(Ψx, Ψy, θ, ω, idx)
-σ = stderr_SparKLIE(Ψx, Ψy, θ, ω)
+    println("step 3")
+    # SparKLIE+1
+    θ1 = KLIEPInference._debias1(Ψx, Ψy, θ, ω, idx)
+    σ = stderr_SparKLIE(Ψx, Ψy, θ, ω)
+    
+    T1 = abs(θ1 / σ) > quantile(Normal(), 0.975) ? 1 : 0
 
-T1 = abs(θ1 / σ) > quantile(Normal(), 0.975) ? 1 : 0
+    # SparKLIE+2
+    θ2 = KLIEPInference._debias2(Ψx, Ψy, θ, ω, idx)
+    supp = KLIEPInference._find_supp(idx, θ, ω)
+    ω = KLIEP_Hessian(θ[supp], Ψy[supp, :])\(supp .=== idx)
+    σ = stderr_SparKLIE(Ψx[supp, :], Ψy[supp, :], θ[supp], ω)
 
-# SparKLIE+2
-θ2 = KLIEPInference._debias2(Ψx, Ψy, θ, ω, idx)
-supp = KLIEPInference._find_supp(idx, θ, ω)
-ω = KLIEP_Hessian(θ[supp], Ψy[supp, :])\(supp .=== idx)
-σ = stderr_SparKLIE(Ψx[supp, :], Ψy[supp, :], θ[supp], ω)
+    T2 = abs(θ2 / σ) > quantile(Normal(), 0.975) ? 1 : 0
 
-T2 = abs(θ2 / σ) > quantile(Normal(), 0.975) ? 1 : 0
+    println("done!")
+    T1, T2
+end
 
-println("saving results to $(scratch_dir)/res_$(set)_$(δ)_$(rep).jld")
-@save "$(scratch_dir)/res_$(set)_$(δ)_$(rep).jld" T1 T2
 
-println("done!")
+function exper2(set, δ, batch, scratch_dir)    
+    println("importing parameters from params_exp2_$(set)_$(δ).jld")
+    file = jldopen("graphs/params_exp2_$(set)_$(δ).jld", "r")
+    γx = read(file, "γx")
+    γy = read(file, "γy")
+    close(file)
+    p = length(γx)
+
+    T1 = zeros(100)
+    T2 = zeros(100)
+
+    start_rep = (batch-1) * 100
+    for rep=1:100
+        Ψx, Ψy = generate_data(γx, γy, start_rep + rep)
+        T1[rep], T2[rep] = test_exper2(Ψx, Ψy, p)
+    end
+
+    println("saving results to $(scratch_dir)/res_$(set)_$(δ)_$(batch).jld")
+    @save "$(scratch_dir)/res_$(set)_$(δ)_$(batch).jld" T1 T2
+    nothing    
+end
+
+exper2(set, δ, batch, scratch_dir)
+
+
+
+
+
